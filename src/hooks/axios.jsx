a4afc1f45f61efa81eac34bd28e8fb8064315d1d
DaurenCS <dau.04.04@mail.com>
2024-05-23 00:35:37 +0600

    last

--- src/hooks/axios.jsx
+++ src/hooks/axios.jsx
@@ -4,5 +4,5 @@ const local = "http://127.0.0.1:8000"
 const renderhost = "https://back-asia-fixing.onrender.com"
 
 export default axios.create({
-    baseURL: `${local}`,
+    baseURL: `${renderhost}`,
   });

2f87008a13c7b435cf755a27419fba94950f4fa2
DaurenCs <d_kozhakhmetov@kbtu.kz>
2024-06-06 21:02:37 +0600

    new

--- src/products.js
+++ src/products.js
@@ -18,14 +18,14 @@ export const tech = [
         "name": "isolation-system",
         "description": "Гидроизоляция",
         "local": "ru",
-        "image": "http://asiafix-pro.com:8000/upload/krystaline.png",
+        "image": "http://asiafix-pro.com:8000/media/uploads/krystaline.png",
         "text": "Добавки для гидроизоляции бетона по технологии CSH"
       },
       {
         "name": "installation-system",
         "description": "Монтажные Системы",
         "local": "ru",
-        "image": "http://asiafix-pro.com:8000/upload/inka.jpg",
+        "image": "http://asiafix-pro.com:8000/media/uploads/inka.jpg",
         "text": "Технология Inka Fixing продолжает упрощать вашу жизнь благодаря своим конструктивным соединениям, оказывая при этом ощутимое влияние на все сферы жизни. Мы готовы помочь вам во всех областях применения наших интеллектуальных решений."
       }
   ]
